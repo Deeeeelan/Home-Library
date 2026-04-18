1e6eb8bd7959e105e4b0e4df0b4764f30d3dcb3c
lraj22 <63673413+lraj22@users.noreply.github.com>
2026-04-18 16:41:14 -0700

    Add login route

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -15,9 +15,12 @@ def create_app(test_config=None):
 
     os.makedirs(app.instance_path, exist_ok=True)
 
+	# init db
     from . import db
-    from .api.auth import auth_bp
     db.init_app(app)
+    
+	# regiser auth
+    from .api.auth import auth_bp
     app.register_blueprint(auth_bp)
 
     @app.route('/hello')
--- flaskr/api/auth.py
+++ flaskr/api/auth.py
@@ -1,39 +1,63 @@
 from flask import Blueprint, render_template, abort, request
-from jinja2 import TemplateNotFound
 import json
+import hashlib
+import jwt
+import dotenv
+import os
+import datetime
+from datetime import timezone
+dotenv.load_dotenv()
+JWT_SECRET = os.environ['JWT_SECRET']
 
 auth_bp = Blueprint('auth', __name__, url_prefix='/api')
 
-@auth_bp.route('/login')
+@auth_bp.route('/login', methods=['POST'])
 def login():
-    data = request.get_json()
-    print(data)
-    
-    username = data["username"]
-    password = data["password"]
-    
-    print("username: ", username)
-    
-    return 'login page!'
+	data = request.json
+	
+	username = data.get("username")
+	password = data.get("password")
+	pwhash = hashlib.sha256(password.encode('utf-8')).hexdigest()
+	
+	print("username: ", username)
+	print("password HASH: ", pwhash)
+	
+	with open("flaskr/data/users.json", "r") as users_fp:
+		users_db = json.load(users_fp)
+		user_found = next(user for user in users_db if ((user['username'] == username and user['pwhash'] == pwhash)))
+		
+		if user_found:
+			now = datetime.datetime.now(tz=timezone.utc)
+			generated_jwt = jwt.encode({
+				'iat': now,
+				'exp': now + datetime.timedelta(days=3),
+				'sub': user_found['id'],
+			}, JWT_SECRET, algorithm='HS256')
+			return json.dumps({
+				'success': True,
+				'jwt': generated_jwt,
+			})
+		else:
+			return json.dumps({
+				'success': False,
+				'jwt': None,
+			})
+	
+	return json.dumps({
+		'success': False,
+		'jwt': None,
+	})
 
 @auth_bp.route('/signup')
 def signup():
-    data = request.get_json()
-    print(data)
-    
-    username = data["username"]
-    password = data["password"]
-    name = data["name"]
+	data = request.get_json()
+	print(data)
+	
+	username = data["username"]
+	password = data["password"]
+	name = data["name"]
 	# TODO: location
-    
-    print("username: ", username)
-    
-    return 'signup page!'
-
-# @auth_bp.route('/', defaults={'page': 'index'})
-# @auth_bp.route('/<page>')
-# def show(page):
-#     try:
-#         return render_template(f'pages/{page}.html')
-#     except TemplateNotFound:
-#         abort(404)
+	
+	print("username: ", username)
+	
+	return 'signup page!'
