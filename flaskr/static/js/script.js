dec47cf763ae832320332198306ddae509dd8c53
Dylan Tseng <108163886+Deeeeelan@users.noreply.github.com>
2026-04-19 11:10:23 -0700

    Update frontend

--- flaskr/static/js/script.js
+++ flaskr/static/js/script.js
@@ -1,1 +1,10 @@
 console.log("js is running")
+
+function search_books() {
+    console.log("searching")
+    const response = fetch("http://127.0.0.1:5000/api/search", {
+        method: "GET",
+        body: JSON.stringify({ zip_code: "01234" }),
+    })
+    console.log(response)
+}
